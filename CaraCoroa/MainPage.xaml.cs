ab0e49975db560a825e12e0cb52bd9a280005101
etechas <etechas@LI02-04>
2024-04-10 18:20:31 -0300

    Add project files.

---
+++ CaraCoroa/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace CaraCoroa
+{
+    public partial class MainPage : ContentPage
+    {
+        int ladoSorteado = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CoinSidesPicker.SelectedIndex = 0;
+        }
+        private void PlayCoinButton_Clicked(object sender, EventArgs e)
+        {
+            //Pegar o número selecionado pelo usuário
+            int max = Convert.ToInt32(CoinSidesPicker.SelectedItem);
+            //Sortear um numero que seja entre 1 e o numero selecionado
+            ladoSorteado = new Random().Next(1, max + 1);
+            //Exibir ele na label da interface
+            CoinResultLabel. = ladoSorteado.ToString();
+        }
+    }
+
+}
